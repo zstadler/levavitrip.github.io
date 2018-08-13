59a74771ce5e93a9668c8b633964bd2a67d3d23a
Yehuda Horn <Yehudah@Taldor.corp>
2018-08-13 14:10:49 +0300

    adding files

---
+++ data/FAQ_1.js
@@ -0,0 +1,8 @@
+var json_FAQ_1 = {
+"type": "FeatureCollection",
+"name": "FAQ_1",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "fid": "1", "Question": "מתי יבשו את האגם?" }, "geometry": { "type": "Point", "coordinates": [ 35.601056694038249, 33.074440839293644 ] } }
+]
+}
