2fb8c311f45469895b1c114ae0637a83bac7535b
Yehuda Horn <Yehudah@Taldor.corp>
2018-08-15 15:02:43 +0300

    adding files

--- data/FAQ_1.js
+++ data/FAQ_1.js
@@ -3,6 +3,13 @@ var json_FAQ_1 = {
 "name": "FAQ_1",
 "crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
 "features": [
-{ "type": "Feature", "properties": { "fid": "1", "Question": "מתי יבשו את האגם?" }, "geometry": { "type": "Point", "coordinates": [ 35.601056694038249, 33.074440839293644 ] } }
+{ "type": "Feature", "properties": { "fid": "Autogenerate", "Question": "איך מכונה הכביש ולשם מה הוא נסלל?", "level": 1.0 }, "geometry": { "type": "Point", "coordinates": [ 35.508586492266353, 33.088839629441246 ] } },
+{ "type": "Feature", "properties": { "fid": "Autogenerate", "Question": "מה הקשר בין משמר הירדן למשואות?", "level": 2.0 }, "geometry": { "type": "Point", "coordinates": [ 35.60157827704824, 33.005467678106918 ] } },
+{ "type": "Feature", "properties": { "fid": "Autogenerate", "Question": "באיזה עלייה הוקמה יסוד המעלה?", "level": 1.0 }, "geometry": { "type": "Point", "coordinates": [ 35.609487372145658, 33.056839491908988 ] } },
+{ "type": "Feature", "properties": { "fid": "Autogenerate", "Question": "לאיזה ממקורות הירדן יש את הספיקה הגדולה ביותר?", "level": 1.0 }, "geometry": { "type": "Point", "coordinates": [ 35.629180220039757, 33.130228386994084 ] } },
+{ "type": "Feature", "properties": { "fid": "1", "Question": "מתי יבשו את האגם?", "level": 1.0 }, "geometry": { "type": "Point", "coordinates": [ 35.601056694038249, 33.074440839293644 ] } },
+{ "type": "Feature", "properties": { "fid": "2", "Question": "מי זה אליעזר שעל שמו נקרא היישוב?", "level": 2.0 }, "geometry": { "type": "Point", "coordinates": [ 35.562671345609765, 33.046207329877234 ] } },
+{ "type": "Feature", "properties": { "fid": "3", "Question": "איזה מנשאי המדינה היה בין מקימי כפר גלעדי?", "level": 2.0 }, "geometry": { "type": "Point", "coordinates": [ 35.575693965820285, 33.242869462910825 ] } },
+{ "type": "Feature", "properties": { "fid": "4", "Question": "איזה מוצר מפורסם מייצרים בקיבוץ?", "level": 1.0 }, "geometry": { "type": "Point", "coordinates": [ 35.597424004875847, 33.160111854162054 ] } }
 ]
 }
