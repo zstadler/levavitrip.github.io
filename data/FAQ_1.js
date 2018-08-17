aa1f78b836d630a295f8abc79dd125523d047a7c
Yehuda Horn <Yehudah@Taldor.corp>
2018-08-17 17:00:50 +0300

    adding files

--- data/FAQ_1.js
+++ data/FAQ_1.js
@@ -1,15 +1,20 @@
 var json_FAQ_1 = {
 "type": "FeatureCollection",
-"name": "FAQ_1",
+"name": "FAQ_0",
 "crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
 "features": [
-{ "type": "Feature", "properties": { "fid": "Autogenerate", "Question": "איך מכונה הכביש ולשם מה הוא נסלל?", "level": 1.0 }, "geometry": { "type": "Point", "coordinates": [ 35.508586492266353, 33.088839629441246 ] } },
-{ "type": "Feature", "properties": { "fid": "Autogenerate", "Question": "מה הקשר בין משמר הירדן למשואות?", "level": 2.0 }, "geometry": { "type": "Point", "coordinates": [ 35.60157827704824, 33.005467678106918 ] } },
-{ "type": "Feature", "properties": { "fid": "Autogenerate", "Question": "באיזה עלייה הוקמה יסוד המעלה?", "level": 1.0 }, "geometry": { "type": "Point", "coordinates": [ 35.609487372145658, 33.056839491908988 ] } },
-{ "type": "Feature", "properties": { "fid": "Autogenerate", "Question": "לאיזה ממקורות הירדן יש את הספיקה הגדולה ביותר?", "level": 1.0 }, "geometry": { "type": "Point", "coordinates": [ 35.629180220039757, 33.130228386994084 ] } },
 { "type": "Feature", "properties": { "fid": "1", "Question": "מתי יבשו את האגם?", "level": 1.0 }, "geometry": { "type": "Point", "coordinates": [ 35.601056694038249, 33.074440839293644 ] } },
 { "type": "Feature", "properties": { "fid": "2", "Question": "מי זה אליעזר שעל שמו נקרא היישוב?", "level": 2.0 }, "geometry": { "type": "Point", "coordinates": [ 35.562671345609765, 33.046207329877234 ] } },
 { "type": "Feature", "properties": { "fid": "3", "Question": "איזה מנשאי המדינה היה בין מקימי כפר גלעדי?", "level": 2.0 }, "geometry": { "type": "Point", "coordinates": [ 35.575693965820285, 33.242869462910825 ] } },
-{ "type": "Feature", "properties": { "fid": "4", "Question": "איזה מוצר מפורסם מייצרים בקיבוץ?", "level": 1.0 }, "geometry": { "type": "Point", "coordinates": [ 35.597424004875847, 33.160111854162054 ] } }
+{ "type": "Feature", "properties": { "fid": "4", "Question": "איזה מוצר מפורסם מייצרים בקיבוץ?", "level": 1.0 }, "geometry": { "type": "Point", "coordinates": [ 35.597424004875847, 33.160111854162054 ] } },
+{ "type": "Feature", "properties": { "fid": "5", "Question": "איך מכונה הכביש ולשם מה הוא נסלל?", "level": 1.0 }, "geometry": { "type": "Point", "coordinates": [ 35.508586492266353, 33.088839629441246 ] } },
+{ "type": "Feature", "properties": { "fid": "6", "Question": "מה הקשר בין משמר הירדן למשואות?", "level": 2.0 }, "geometry": { "type": "Point", "coordinates": [ 35.60157827704824, 33.005467678106918 ] } },
+{ "type": "Feature", "properties": { "fid": "7", "Question": "באיזה עלייה הוקמה יסוד המעלה?", "level": 1.0 }, "geometry": { "type": "Point", "coordinates": [ 35.609487372145658, 33.056839491908988 ] } },
+{ "type": "Feature", "properties": { "fid": "8", "Question": "לאיזה ממקורות הירדן יש את הספיקה הגדולה ביותר?", "level": 1.0 }, "geometry": { "type": "Point", "coordinates": [ 35.629180220039757, 33.130228386994084 ] } },
+{ "type": "Feature", "properties": { "fid": "9", "Question": "מי זה נבי יושע?", "level": 2.0 }, "geometry": { "type": "Point", "coordinates": [ 35.556859933807964, 33.113049568029318 ] } },
+{ "type": "Feature", "properties": { "fid": "10", "Question": "מאיזה מדינה הגיעו מייסדי ראש פינה", "level": 2.0 }, "geometry": { "type": "Point", "coordinates": [ 35.54049914319129, 32.968219236988602 ] } },
+{ "type": "Feature", "properties": { "fid": "11", "Question": "מה הגובה של הר מתת?", "level": 1.0 }, "geometry": { "type": "Point", "coordinates": [ 35.35461471727529, 33.038893098285008 ] } },
+{ "type": "Feature", "properties": { "fid": "12", "Question": "בן כמה היה סבא עמנואל בעליה לבריה?", "level": 1.0 }, "geometry": { "type": "Point", "coordinates": [ 35.509212979446694, 32.990234046441529 ] } },
+{ "type": "Feature", "properties": { "fid": "13", "Question": "איך קוראים לבסיס שיאיר עשה בו צמ\"פ", "level": 1.0 }, "geometry": { "type": "Point", "coordinates": [ 35.724549904206071, 33.044196052522885 ] } }
 ]
 }
